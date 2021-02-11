945051d0bf3b62dee577603ca205a064be43dfa0
sagarverma <intern.sagar.verma@firstcry.in>
2021-02-11 23:47:05 +0530

    Routing and Nav UI Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { BasicFormComponent } from './basic-form/basic-form.component';
+import { ReactiveDynamicFormComponent } from './reactive-dynamic-form/reactive-dynamic-form.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { StarterReactiveFormComponent } from './starter-reactive-form/starter-reactive-form.component';
+import { StarterTemplateFormComponent } from './starter-template-form/starter-template-form.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
+import { UpdateOnComponent } from './update-on/update-on.component';
+import { CustomerComponentsComponent } from './customer-components/customer-components.component';
+
+const routes: Routes = [
+  {path: '', pathMatch:'full', redirectTo: '/basic-form'},
+  {path:'basic-form', component:BasicFormComponent},
+  {path:'reactive-dynamic-form', component:ReactiveDynamicFormComponent},
+  {path:'reactive-form', component:ReactiveFormComponent},
+  {path:'starter-reactive-form', component:StarterReactiveFormComponent},
+  {path:'starter-template-form', component:StarterTemplateFormComponent},
+  {path:'template-form', component:TemplateFormComponent},
+  {path:'update-on', component:UpdateOnComponent},
+  {path:'customer-component', component:CustomerComponentsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
